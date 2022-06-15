73edfc1ea93e1bce918f6f5b82e9113ea0128961
DeVector <diniz.vito@gmail.com>
2022-06-15 12:04:53 -0300

    Add class beer

---
+++ src/main/java/com/beer/project/model/Beer.java
@@ -0,0 +1,37 @@
+package com.beer.project.model;
+
+import com.beer.project.model.enums.BeerType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_beer")
+public class Beer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String brand;
+
+    @Column(nullable = false)
+    private int max;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BeerType beerType;
+
+}
