2e68ba73597161028227f4b17a0eebc67d499911
DeVector <diniz.vito@gmail.com>
2022-06-15 17:27:16 -0300

    Add config to swagger

---
+++ src/main/java/com/beer/project/config/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.beer.project.config;
+
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.RequestHandlerSelectors.*;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private final String BASE_PACKAGE = "com.beer.project.controller";
+    private final String API_TITLE = "Beer Stock API";
+    private final String API_DESCRIPTION = "REST API for beer stock management";
+    private final String CONTACT_NAME = "Victor Oliveira";
+    private final String CONTACT_GITHUB = "https://github.com/DeVector/";
+    private final String CONTACT_EMAIL = "diniz.vito@gmail.com";
+
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(basePackage(BASE_PACKAGE))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(buildApiInfo());
+    }
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_TITLE)
+                .description(API_DESCRIPTION)
+                .version("1.0.1")
+                .contact(new Contact(CONTACT_NAME, CONTACT_GITHUB, CONTACT_EMAIL))
+                .build();
+    }
+}
