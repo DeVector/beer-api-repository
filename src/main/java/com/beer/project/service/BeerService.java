977adbb49c4a92ffaa783fef2c206b0fd901e25b
DeVector <diniz.vito@gmail.com>
2022-06-15 17:51:58 -0300

    Add Delete, FindAll, FindName and Putch in the service

--- src/main/java/com/beer/project/service/BeerService.java
+++ src/main/java/com/beer/project/service/BeerService.java
@@ -2,6 +2,7 @@ package com.beer.project.service;
 
 import com.beer.project.exception.BeerAlreadyRegisteredException;
 import com.beer.project.exception.BeerNotFoundException;
+import com.beer.project.exception.BeerStockExceededException;
 import com.beer.project.mapper.BeerMapper;
 import com.beer.project.model.Beer;
 import com.beer.project.model.dtos.BeerDTO;
@@ -10,7 +11,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor(onConstructor = @__(@Autowired))
@@ -29,6 +32,24 @@ public class BeerService {
 
     }
 
+    public BeerDTO findByName(String name) throws BeerNotFoundException {
+        Beer beer = repository.findByName(name)
+                .orElseThrow(() -> new BeerNotFoundException(name));
+        return mapper.toDTO(beer);
+    }
+
+    public List<BeerDTO> findAll() {
+        return repository.findAll()
+                .stream()
+                .map(mapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public void deleteById(Long id) throws BeerNotFoundException {
+        verifyIfExists(id);
+        repository.deleteById(id);
+    }
+
     private void verifyIfExistsRegistered(String name) throws BeerAlreadyRegisteredException {
 
         Optional<Beer> optional = repository.findByName(name);
@@ -36,5 +57,21 @@ public class BeerService {
             throw new BeerAlreadyRegisteredException(name);
         }
 
+    }
+
+    private Beer verifyIfExists(Long id) throws BeerNotFoundException {
+        return repository.findById(id)
+                .orElseThrow(() -> new BeerNotFoundException(id));
+    }
+
+    public BeerDTO increment(Long id, int quantityToIncrement) throws BeerNotFoundException, BeerStockExceededException {
+        Beer beerToIncrement = verifyIfExists(id);
+        int quantityAfterIncrement = quantityToIncrement + beerToIncrement.getQuantity();
+        if (quantityAfterIncrement <= beerToIncrement.getMax()) {
+            beerToIncrement.setQuantity(beerToIncrement.getQuantity() + quantityToIncrement);
+            Beer incrementBeer = repository.save(beerToIncrement);
+            return mapper.toDTO(incrementBeer);
+        }
+        throw new BeerStockExceededException(id, quantityToIncrement);
     }
 }
