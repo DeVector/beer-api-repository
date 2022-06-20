6d4f65a822858d37c58e80944726de323ab8e7b6
DeVector <diniz.vito@gmail.com>
2022-06-20 10:55:25 -0300

    Add controller test create

---
+++ src/test/java/com/beer/project/controller/BeerControllerTest.java
@@ -0,0 +1,73 @@
+package com.beer.project.controller;
+
+import com.beer.project.builder.BeerDTOBuilder;
+import com.beer.project.exception.BeerAlreadyRegisteredException;
+import com.beer.project.model.dtos.BeerDTO;
+import com.beer.project.service.BeerService;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static com.beer.project.utils.JsonConvertionUtils.asJsonString;
+
+@ExtendWith(MockitoExtension.class)
+public class BeerControllerTest {
+
+    private static final String BEER_API_URL_PATH = "/api/v1/beers";
+    private static final long VALID_BEER_ID = 1L;
+    private static final long INVALID_BEER_ID = 2L;
+    private static final String BEER_API_SUBPATH_INCREMENT_URL = "/increment";
+    private static final String BEER_API_SUBPATH_DECREMENT_URL = "/decrement";
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private BeerService service;
+
+    @InjectMocks
+    private BeerController controller;
+
+    @BeforeEach
+    void setUp() {
+
+        mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
+                .setViewResolvers((viewName, locale) -> new MappingJackson2JsonView())
+                .build();
+
+    }
+
+    @Test
+    void whenPostIsCalledThenABeerCreated() throws Exception {
+
+        BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+
+        when(service.createBeer(beerDTO)).thenReturn((beerDTO));
+
+        mockMvc.perform(post(BEER_API_URL_PATH)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(asJsonString(beerDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.name", is(beerDTO.getName())))
+                .andExpect(jsonPath("$.brand", is(beerDTO.getBrand())));
+
+    }
+
+}
--- src/test/java/com/beer/project/service/BeerServiceTest.java
+++ src/test/java/com/beer/project/service/BeerServiceTest.java
@@ -6,7 +6,11 @@ import com.beer.project.mapper.BeerMapper;
 import com.beer.project.model.Beer;
 import com.beer.project.model.dtos.BeerDTO;
 import com.beer.project.repositories.BeerRepository;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
+
+import static org.hamcrest.MatcherAssert.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,7 +21,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
+import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class BeerServiceTest {
@@ -36,13 +42,27 @@ public class BeerServiceTest {
         BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
         Beer expectedSavedBeer = mapper.toModel(beerDTO);
 
-        Mockito.when(repository.findByName(beerDTO.getName())).thenReturn(Optional.empty());
-        Mockito.when(repository.save(expectedSavedBeer)).thenReturn(expectedSavedBeer);
+        when(repository.findByName(beerDTO.getName())).thenReturn(Optional.empty());
+        when(repository.save(expectedSavedBeer)).thenReturn(expectedSavedBeer);
 
         BeerDTO createdBeerDTO = service.createBeer(beerDTO);
 
-        
-        assertEquals(beerDTO.getName(), createdBeerDTO.getName());
+        assertThat(createdBeerDTO.getId(), is(equalTo(beerDTO.getId())));
+        assertThat(createdBeerDTO.getName(), is(equalTo(beerDTO.getName())));
+        assertThat(createdBeerDTO.getQuantity(), is(equalTo(beerDTO.getQuantity())));
+
+    }
+
+    @Test
+    void whenAlreadyRegisterBeerInformedThenExceptionShouldBeThrow() {
+
+        BeerDTO beerDTO = BeerDTOBuilder.builder().build().toBeerDTO();
+        Beer duplicateBeer = mapper.toModel(beerDTO);
+
+        when(repository.findByName(beerDTO.getName())).thenReturn(Optional.of(duplicateBeer));
+
+        assertThrows(BeerAlreadyRegisteredException.class, () -> service.createBeer(beerDTO));
+
     }
 
 }
